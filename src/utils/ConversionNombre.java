e47bac4178d7a333c8fdbae16b44dfd7d7d71915
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-10 17:03:15 +0200

    Add Conversion Nombre

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,22 @@
+package utils;
+
+/**
+ * TP4
+ * 
+ * @author Jeremy
+ *
+ */
+public class ConversionNombre {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String aNumber = "2";
+		int aString = Integer.parseInt(aNumber);
+		
+		System.out.println(aString);
+
+	}
+
+}
