8ddcb270f8b3bf5fd4a7bc5aaca19354204d7ee8
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-10 11:34:15 +0200

    Init project

---
+++ src/fr/entites/AdressePostale.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.entites;
+
+/**
+ * @author Jeremy
+ *
+ */
+public class AdressePostale {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
