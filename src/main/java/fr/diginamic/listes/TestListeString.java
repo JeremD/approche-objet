f12fc0c81af8100e6e8cccff6e676b10471ee89c
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-16 23:17:13 +0200

    ArrayList - Liste de chaîne

--- src/main/java/fr/diginamic/listes/TestListeString.java
+++ src/main/java/fr/diginamic/listes/TestListeString.java
@@ -1,6 +1,7 @@
 package fr.diginamic.listes;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -17,21 +18,39 @@ public class TestListeString {
 	 */
 	public static void main(String[] args) {
 
-		// Liste d'entiers
+		// Liste stockant les villes
+		String[] villes = {"Nice", "Carcassonne", "Narbonne", "Lyon", "Foix", "Pau", "Marseille", "Tarbes"};
 		List<String> listeChar = new ArrayList<String>();
+		listeChar.addAll(Arrays.asList(villes));
 		
-		listeChar.addAll("Nice", "Carcassonne", "Narbonne", "Lyon", "Foix", "Pau", "Marseille", "Tarbes");
-
 		// Nom de ville la plus longue
-		Iterator<String> iterator = listeChar.iterator();
-
-		while (iterator.hasNext()) {
-			String ville = (String) iterator.next();
-			
-			/*if (ville.length() < ) {
-				
-			}*/
+		String villeLongue = listeChar.get(0);
+		
+		for (String nomVille : listeChar) {
+			if (villeLongue.length() < nomVille.length() ) {
+				villeLongue = nomVille;	
+			}
 		}
+		System.out.println("Ville avec le plus grand nombre de lettre : " + villeLongue);
+		
+		// Nom des villes en majuscule
+		for (int i = 0; i < listeChar.size(); i++) {
+			String nomVille = listeChar.get(i);
+			String nomVilleMaj = nomVille.toUpperCase();
+			listeChar.set(i, nomVilleMaj);
+		}
+		System.out.println("Ville en majuscules : " + listeChar);
+
+		// Suppression villes commençant par la lettre N
+		Iterator<String> iterator = listeChar.iterator();
+		
+		while (iterator.hasNext()) {
+			String nomVille = iterator.next();
+			if (nomVille.startsWith("N")) {
+				iterator.remove();
+			}
+		}
+		System.out.println("Nouvelle liste des villes : " + listeChar);
 
 	}
 
