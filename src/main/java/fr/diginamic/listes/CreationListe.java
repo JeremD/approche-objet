671b98922d8f1821fe848ab37dfc04fff1171e9e
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-16 14:27:32 +0200

    ArrayList - Création Liste

---
+++ src/main/java/fr/diginamic/listes/CreationListe.java
@@ -0,0 +1,28 @@
+package fr.diginamic.listes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jeremy
+ *
+ */
+public class CreationListe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		List<Integer> maListe = new ArrayList<>();
+		
+		// Ajout de tous les nombres de 1 à 100
+		for (int i = 1; i <= 100; i++) {
+			maListe.add(i);
+		}
+		
+		System.out.println(maListe);
+		
+	}
+
+}
