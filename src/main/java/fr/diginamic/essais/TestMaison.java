d9487e15ab5c45bf762df3696f903579f3c6456e
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-18 00:36:34 +0200

    Autoformation: Immobilier (avec aide correction)

--- src/main/java/fr/diginamic/essais/TestMaison.java
+++ src/main/java/fr/diginamic/essais/TestMaison.java
@@ -23,20 +23,23 @@ public class TestMaison {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		// Création d'une maison
 		Maison maison = new Maison();
 
-		// Ajout des pièces
-		maison.ajouterPiece(new Chambre(9.5, 0));
+		// Ajout des pièces (superficie + étage)
 		maison.ajouterPiece(new Cuisine(15.5, 0));
-		maison.ajouterPiece(new SalleDeBain(2.5, 0));
-		maison.ajouterPiece(new WC(1.50, 0));
-		maison.ajouterPiece(new Salon(30.5, 0));
-		maison.ajouterPiece(new SalleDeBain(4.5, 1));
+		maison.ajouterPiece(new Salon(30, 0));
+		maison.ajouterPiece(new Chambre(10, 1));
+		maison.ajouterPiece(new Chambre(7, 1));
+		maison.ajouterPiece(new SalleDeBain(4, 1));
+		maison.ajouterPiece(new WC(1.50, 1));
 
-		System.out.println("Superficie Total: " + maison.calculerSurface());
-		System.out.println("Superficie de l'étage 0: " + maison.superficieEtage(0));
-		System.out.println("Superficie de l'étage 1: " + maison.superficieEtage(1));
-		System.out.println("Superficie des chambres: " + maison.superficieTypePiece(Piece.TYPE_CHAMBRE));
+		// Affichage de la maison
+		System.out.println("Superficie de l'étage 0 : " + maison.superficieEtage(0));
+		System.out.println("Superficie de l'étage 1 : " + maison.superficieEtage(1));
+		System.out.println("Superficie de la chambre : " + maison.superficieTypePiece(Piece.TYPE_CHAMBRE));
+		System.out.println("Superficie Total : " + maison.supercifieTotal());
 	}
 
 }
