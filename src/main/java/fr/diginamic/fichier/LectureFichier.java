b8b1479314c862216d9ead1fca64770d79f9063a
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-18 12:00:01 +0200

    Lecture Fichier

---
+++ src/main/java/fr/diginamic/fichier/LectureFichier.java
@@ -0,0 +1,44 @@
+package fr.diginamic.fichier;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
+
+import java.io.IOException;
+
+/**
+ * TP Lecture Fichier
+ * 
+ * @author Jeremy
+ *
+ */
+public class LectureFichier {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		try {
+			// Déclaration du fichier à lire
+			File csvFile = new File("E:/Documents/DEV/tp 6 - DATA - recensement 2010.csv");
+
+			// Lecture du fichier
+			List<String> csvLignes = FileUtils.readLines(csvFile, "UTF-8");
+
+			for (String read : csvLignes) {
+				System.out.println(read);
+
+			}
+			System.out.println("Nombre de lignes : " + csvLignes.size());
+
+		} catch (IOException e) {
+
+			// Affichage exception
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
