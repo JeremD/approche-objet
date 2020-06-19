db7e8f236ebf78004bfd74dd6b09edbd14296635
JeremYD <delpech.jeremy@hotmail.fr>
2020-06-19 17:47:36 +0200

    Gestion des Exceptions

--- src/main/java/fr/diginamic/testexceptions/TestReflectionUtils.java
+++ src/main/java/fr/diginamic/testexceptions/TestReflectionUtils.java
@@ -2,6 +2,7 @@ package fr.diginamic.testexceptions;
 
 import fr.diginamic.exceptions.ReflectionException;
 import fr.diginamic.sets.Pays;
+import fr.diginamic.testenumeration.Continent;
 
 /**
  * Gestion des Exceptions
@@ -16,10 +17,10 @@ public class TestReflectionUtils {
 	 */
 	public static void main(String[] args) {
 
-		Pays exceptPays = new Pays("Tamalou", 1_800_000, 6597);
+		Pays exceptPays = new Pays("Tamalou", 1_800_000, 6597, Continent.OCEANIE);
 
 		try {
-			ReflectionUtils.afficherAttributs(null);
+			ReflectionUtils.afficherAttributs(exceptPays);
 		} catch (IllegalArgumentException | IllegalAccessException | ReflectionException e) {
 			System.out.println(e.getMessage());
 		}
